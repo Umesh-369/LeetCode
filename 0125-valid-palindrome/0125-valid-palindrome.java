1a64614734205333e3bf88553743916b69d59052
Umesh Sai Hanuma Prasad Syamala <s.umeshsaihanumaprasad@gmail.com>
2026-05-22 16:59:24 +0530

    Time: 14 ms (31.15%), Space: 46.5 MB (22.52%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+             s=s.toLowerCase();
+             String str=s.replaceAll("[^a-zA-Z0-9]","");
+             for(int i=0;i<str.length()/2;i++){
+                if(str.charAt(i)!=str.charAt(str.length()-i-1)){
+                    return false;
+                }
+             }
+             return true;
+    }
+}
