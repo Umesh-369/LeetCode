0e326c5135dbb79d444eb225e1a15e131b71c349
Umesh Sai Hanuma Prasad Syamala <s.umeshsaihanumaprasad@gmail.com>
2026-06-01 16:14:18 +0530

    Time: 0 ms (100%), Space: 43.8 MB (9.58%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void sortColors(int[] nums) {
+       int n=nums.length;
+       int[] arr=new int[3];
+       for(int i=0;i<n;i++){
+        arr[nums[i]]=arr[nums[i]]+1;
+       } 
+       int z=arr[0];
+       int o=arr[1];
+       int t=arr[2];
+       for(int i=0;i<z;i++){
+        nums[i]=0;
+       }
+       
+       for(int i=z;i<n-t;i++){
+          nums[i]=1;
+       }
+       for(int i=z+o;i<n;i++){
+        nums[i]=2;
+       }
+
+       
+    }
+}
