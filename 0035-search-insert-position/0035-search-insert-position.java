afee2aef9b6c92ad6e1191b5bf4f1fdbb66447c7
Umesh Sai Hanuma Prasad Syamala <s.umeshsaihanumaprasad@gmail.com>
2026-07-20 09:58:04 +0530

    Time: 0 ms (100%), Space: 44.5 MB (92.84%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,21 +1,23 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int start=0;
-        int end=nums.length-1;
-        int ans=0;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(nums[mid]==target){
-                return mid;
-            }
-            else if(nums[mid]<target){
-                start=mid+1;
-            }
-            else{
-                end=mid-1;
-            }
-
+       int n=nums.length;
+       int left=0;
+       int right=n-1;
+      
+       while(left<=right){
+        int mid=left+(right-left)/2;
+        if(nums[mid]==target){
+            return mid;
         }
-        return start;
+        else if(nums[mid]<target){
+            left=mid+1;
+        }
+        else{
+            right=mid-1;
+        }
+       
+       } 
+
+       return left;
     }
 }
