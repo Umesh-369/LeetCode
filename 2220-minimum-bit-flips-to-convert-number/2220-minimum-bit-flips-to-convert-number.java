e1fa06b2954d9cae6e1ae4805091c2d49d90720a
Umesh Sai Hanuma Prasad Syamala <s.umeshsaihanumaprasad@gmail.com>
2026-07-21 09:27:25 +0530

    Time: 1 ms (5.99%), Space: 42.3 MB (36.82%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
@@ -2,9 +2,15 @@ class Solution {
     public int minBitFlips(int start, int goal) {
         int ans=start^goal;
         int count=0;
-
-        for(int i=0;i<32;i++){
-            if((ans & (1<<i))!=0){
+        String bin=Integer.toBinaryString(ans);
+        // for(int i=0;i<32;i++){
+        //     if((ans & (1<<i))!=0){
+        //         count++;
+        //     }
+        // }
+        
+        for(int i=0;i<bin.length();i++){
+            if(bin.charAt(i)=='1'){
                 count++;
             }
         }
