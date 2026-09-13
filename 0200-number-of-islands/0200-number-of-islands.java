2ac40e2b37d815ebaf39f796fe189561a424e04a
Umesh Sai Hanuma Prasad Syamala <s.umeshsaihanumaprasad@gmail.com>
2026-09-13 17:56:57 +0530

    Time: 3 ms (87.44%), Space: 52.1 MB (66.94%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -2,9 +2,9 @@ class Solution {
     public int numIslands(char[][] grid) {
         int m=grid.length;
         int n=grid[0].length;
+
         int[][] visited=new int[m][n];
         int count=0;
-
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]=='1' && visited[i][j]==0){
@@ -13,20 +13,24 @@ class Solution {
                 }
             }
         }
+
         return count;
     }
 
     public void dfs(char[][] grid,int r,int c,int[][] visited){
         int m=grid.length;
         int n=grid[0].length;
-        if(r<0 || r>=m || c<0 || c>=n || visited[r][c]==1 || grid[r][c]=='0')return ;
-       
-        grid[r][c]='0';
+
+        if(r<0 || r>=m || c<0 || c>=n || visited[r][c]==1 || grid[r][c]=='0'){
+            return ;
+        }
+
         visited[r][c]=1;
+        grid[r][c]='0';
+
         dfs(grid,r-1,c,visited);
         dfs(grid,r,c+1,visited);
         dfs(grid,r+1,c,visited);
         dfs(grid,r,c-1,visited);
-          
     }
 }
