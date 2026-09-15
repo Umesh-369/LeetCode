471d88cd669ae6c8f13eac40a8e0ac161a5da35a
Umesh Sai Hanuma Prasad Syamala <s.umeshsaihanumaprasad@gmail.com>
2026-09-15 11:45:32 +0530

    Time: 0 ms (100%), Space: 48.1 MB (33.41%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,6 +1,22 @@
 class Solution {
     public double myPow(double x, int n) {
-        double ans=Math.pow(x,n);
-        return ans;
+      double ans=1;
+      double nn=n;
+      if(nn<0) nn=nn*-1;
+
+      while(nn>0){
+        if(nn%2!=0){
+            ans=ans*x;
+            nn=nn-1;
+        }
+        else{
+            x=x*x;
+            nn=nn/2;
+        }
+      }   
+      if(n<0){
+        ans=1/ans;
+      }
+      return ans;
     }
 }
