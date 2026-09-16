dd64a6bcd3d136e008b4c0505d0fa1962dd5498b
Umesh Sai Hanuma Prasad Syamala <s.umeshsaihanumaprasad@gmail.com>
2026-09-16 21:14:34 +0530

    Time: 2 ms (94.27%), Space: 45.9 MB (33.02%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,25 +1,28 @@
 class Solution {
-    public void findallcomb(int ind,int[] arr,int target,List<List<Integer>> ans,List<Integer> ds){
-       if(ind==arr.length){
-          if(target==0){
-            ans.add(new ArrayList<>(ds));
-          }
-          return ;
-       }
 
-       if(arr[ind]<=target){
-        ds.add(arr[ind]);
-        findallcomb(ind,arr,target-arr[ind],ans,ds);
-        ds.remove(ds.size()-1);
-       }
-       findallcomb(ind+1,arr,target,ans,ds);
+    public void findallcomb(int index,int[] arr,int target,List<List<Integer>> ans,List<Integer> ds){
+        if(index==arr.length){
+            if(target==0){
+               ans.add(new ArrayList<>(ds));
+            }
+            return ;
+        }
 
+        if(arr[index]<=target){
+            ds.add(arr[index]);
+
+            findallcomb(index,arr,target-arr[index],ans,ds);
+
+            ds.remove(ds.size()-1);
+        }
+            findallcomb(index+1,arr,target,ans,ds);
     }
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans=new ArrayList<>();
-        List<Integer> ds=new ArrayList<>();
-        findallcomb(0,candidates,target,ans,ds);
-        return ans;
+      List<List<Integer>> ans=new ArrayList<>();
+      List<Integer> ds=new ArrayList<>();
+      findallcomb(0,candidates,target,ans,ds);
+
+      return ans;   
     }
 }
